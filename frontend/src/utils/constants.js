ffdd6e562d2546980b399f8d23b72a1f69158481
Krasilnikov.M.A. <58246636+rootCurse@users.noreply.github.com>
2026-04-16 00:54:00 +0300

    fix: correct API URL to nomorepartiessite.ru

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://api.kma.nomorepartiessbs.ru";
+export const URL = "https://api.kma.nomorepartiessite.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
